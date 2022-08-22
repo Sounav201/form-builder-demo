f6fd53138056eb867561706927d8ac5b33511562
Sounav201 <sounav2001saha@gmail.com>
2022-08-22 21:01:10 +0530

    Added login page ui

---
+++ pages/builder.tsx
@@ -0,0 +1,252 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Cookies from "js-cookie";
+import ElementBank from '../components/element-bank/ElementBank'
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { FormAreaItem } from "../components/form-area/FormArea.types";
+import { ElementAttributes } from "../components/element-bank/ElementBank.types";
+import { useCallback, useEffect, useState } from "react";
+import { nanoid } from "nanoid";
+import {
+  FORM_ITEMS_SESSION_KEY,
+  SessionStorageService,
+} from "../services/SessionStorageService";
+import FormArea from '../components/form-area/FormArea';
+import ElementProperties from '../components/element-properties/ElementProperties';
+import { FormControl, FormLabel, Select, Stack, Switch, useDisclosure, Button } from '@chakra-ui/react'
+import Sidebar from '../components/element-properties/Sidebar';
+import { parseCookies } from '../services/parseCookies';
+
+const Home: NextPage = ({initialformAreaItems}:any) => {
+
+  const [selectedItem, setSelectedItem] = useState(
+    null as FormAreaItem<ElementAttributes> | null);
+
+  const [formAreaItems, setFormAreaItems] = useState( () =>  initialformAreaItems!= undefined && JSON.parse(initialformAreaItems) || [] as FormAreaItem<ElementAttributes>[]);
+
+  const [showSidebar, setshowSidebar] = useState(false);
+
+  useEffect(() => {
+   // Cookies.set("formAreaItems", JSON.stringify(formAreaItems))
+  }, [formAreaItems])
+  
+
+
+  //Move Form Element
+  const moveItem = useCallback((dragIndex:number, hoverIndex:number) => {
+      setFormAreaItems((oldArray:any) => {
+        const draggedItem = oldArray[dragIndex];
+        console.log("Dragged Item : " ,draggedItem);
+        const newArray =[...oldArray];
+
+
+        return [...newArray]
+
+      });
+
+  },[])
+
+
+  //Highlight when a question is selected  
+  const onQuestionSelected = (item: FormAreaItem<ElementAttributes>) => {
+    setFormAreaItems((oldArray:any) => {
+      const index = oldArray.findIndex((i:any) => i.id === item.id);
+      if (index !== -1) {
+        oldArray.forEach((item:any) => (item.isSelected = false));
+        oldArray[index].isSelected = true;
+      }
+      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
+      return [...oldArray];
+    });
+    setSelectedItem(item);
+  };
+
+  //Drop the form element on to Form Area
+  const onFormAreaDrop = useCallback(
+    (item: FormAreaItem<ElementAttributes>) => {
+      setFormAreaItems((oldArray:any) => {
+        oldArray.forEach((item:any) => (item.isSelected = false));
+        const newArray = [
+          ...oldArray,
+          { ...item, id: nanoid(), isSelected: true },
+        ];
+        // SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, newArray);
+        return [...newArray];
+      });
+    },
+    [formAreaItems]
+  );
+
+  //Remove a form element from Form Area
+  const onElementDelete = (item: FormAreaItem<ElementAttributes>) => {
+    setFormAreaItems((oldArray:any) => {
+      oldArray.forEach((item:any) => (item.isSelected = false));
+      const newArray = oldArray.filter((oldItem:any) => oldItem.id !== item.id);
+      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, newArray);
+      return [...newArray];
+    });
+  };
+
+  //Allow the user to type in their Question to a form element
+  const onElementQuestionChanged = (
+    item: FormAreaItem<ElementAttributes>,
+    questionText: string
+  ) => {
+    setFormAreaItems((oldArray:any) => {
+      const index = oldArray.findIndex((i:any) => i.id === item.id);
+      if (index !== -1) {
+        oldArray[index].question = questionText;
+      }
+      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
+      return [...oldArray];
+    });
+  };
+
+  //Allow the user to update Options to their Checkbox or MCQ component
+  const onOptionEdit = (
+    item: FormAreaItem<ElementAttributes>,
+    option: string,
+    choiceIndex: number,
+  ) => {
+    setFormAreaItems((oldArray:any) => {
+      const index = oldArray.findIndex((i:any) => i.id === item.id);
+      if (index !== -1 && item.type == 'checkbox') {
+        const newChoice = { label: option, value: option , id:oldArray[index].attributes.choices[choiceIndex].id}
+        oldArray[index].attributes.choices[choiceIndex] = newChoice
+   //     console.log(oldArray[index].attributes.choices[choiceIndex])
+      }
+      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
+      return [...oldArray];
+    });
+  };
+
+  //Allow the user to add Options to their Checkbox or MCQ component
+  const onOptionAdd = (
+    item:FormAreaItem<ElementAttributes>,
+
+  ) => {
+    setFormAreaItems((oldArray:any) => {
+      const newChoice = {label:"Type an Option" ,value:"Type an Option", id:nanoid() }
+
+      const index = oldArray.findIndex((i:any) => i.id === item.id);
+      if(index!==-1)
+      {
+        //oldArray[index].attributes.choices= [...oldArray[index].attributes.choices, newChoice];
+
+        oldArray[index].attributes.choices.push(newChoice)
+       //console.log(oldArray[index]);
+      
+      }
+
+      return [...oldArray]
+    });
+
+  };
+
+  const onOptionDelete = (
+    item:FormAreaItem<ElementAttributes>,
+    choiceIndex: number,
+  ) => {
+    setFormAreaItems((oldArray:any) => {
+      const index = oldArray.findIndex((i:any) => i.id === item.id);
+
+      if(index!=-1 && item.type=="checkbox")
+      { 
+        const oldChoices = oldArray[index].attributes.choices;
+        const choiceToDelete = oldChoices[choiceIndex]
+        const newChoices = oldChoices.filter((oldChoice:any)=> oldChoice.id!=choiceToDelete.id)
+
+//        console.log("New choices : ", newChoices);
+        oldArray[index].attributes.choices = newChoices;
+      }
+      
+
+      return [...oldArray]
+    });
+
+  };
+
+  const moveCard = (dragIndex: number, hoverIndex: number) => {
+    const items: any[] = JSON.parse(JSON.stringify(formAreaItems));
+    const itemToMove = items[dragIndex];
+    items.splice(dragIndex, 1);
+    items.splice(hoverIndex, 0, itemToMove);
+    setFormAreaItems(items);
+  };
+
+
+
+  return (
+    <div>
+      <Head>
+        <title>Form Builder Demo</title>
+        <meta name="description" content="Generated by create next app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <DndProvider backend={HTML5Backend}>
+        <div className='h-full min-h-screen bg-slate-400'>
+          <div className='grid grid-cols-12 gap-x-4 '>
+
+            {/*Element Bank  */}
+            <div className='col-span-2 box-border'>
+              <ElementBank />
+
+            </div>
+            {/*Form Area  */}
+            <div className='col-span-8 box-border'>
+              <FormArea
+                moveCard={moveCard}
+                items={formAreaItems}
+                onDrop={onFormAreaDrop}
+                onItemDelete={onElementDelete}
+                onQuestionTextChange={onElementQuestionChanged}
+                onQuestionSelected={onQuestionSelected}
+                setshowSidebar={() => { setshowSidebar(true) }}
+                onOptionEdit={onOptionEdit}
+                onOptionAdd={onOptionAdd}
+                onOptionDelete={onOptionDelete}
+                moveItem={moveItem}
+              />
+
+            </div>
+            {/*Element Properties  */}
+            <div className='col-span-2'>
+              <Sidebar
+                
+                selectedItem={selectedItem}
+                onItemPropertiesChange={(item) => {
+                  setFormAreaItems((oldArray:any) => {
+                    const index = oldArray.findIndex((i:any) => i.id === item.id);
+                    if (index !== -1) {
+                      oldArray[index].attributes.styling = item.attributes.styling;
+                    }
+                    //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
+                    return [...oldArray];
+                  });
+                }}
+                setshowSidebar={() => { setshowSidebar(false) }}
+                sidebarStatus={showSidebar}
+              //isSelected={selectedItem.isSelected}
+              />
+
+            </div>
+
+          </div>
+        </div>
+      </DndProvider>
+    </div>
+  )
+}
+
+Home.getInitialProps = ({req}:any ) => {
+
+  const cookies = parseCookies(req);
+
+  return {initialformAreaItems: cookies.formAreaItems};
+
+  
+};
+
+export default Home
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,252 +1,106 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import Cookies from "js-cookie";
-import ElementBank from '../components/element-bank/ElementBank'
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend";
-import { FormAreaItem } from "../components/form-area/FormArea.types";
-import { ElementAttributes } from "../components/element-bank/ElementBank.types";
-import { useCallback, useEffect, useState } from "react";
-import { nanoid } from "nanoid";
-import {
-  FORM_ITEMS_SESSION_KEY,
-  SessionStorageService,
-} from "../services/SessionStorageService";
-import FormArea from '../components/form-area/FormArea';
-import ElementProperties from '../components/element-properties/ElementProperties';
-import { FormControl, FormLabel, Select, Stack, Switch, useDisclosure, Button } from '@chakra-ui/react'
-import Sidebar from '../components/element-properties/Sidebar';
-import { parseCookies } from '../services/parseCookies';
-
-const Home: NextPage = ({initialformAreaItems}:any) => {
-
-  const [selectedItem, setSelectedItem] = useState(
-    null as FormAreaItem<ElementAttributes> | null);
-
-  const [formAreaItems, setFormAreaItems] = useState( () =>  initialformAreaItems!= undefined && JSON.parse(initialformAreaItems) || [] as FormAreaItem<ElementAttributes>[]);
-
-  const [showSidebar, setshowSidebar] = useState(false);
-
-  useEffect(() => {
-   // Cookies.set("formAreaItems", JSON.stringify(formAreaItems))
-  }, [formAreaItems])
-  
+import { useState } from 'react'
 
 
-  //Move Form Element
-  const moveItem = useCallback((dragIndex:number, hoverIndex:number) => {
-      setFormAreaItems((oldArray:any) => {
-        const draggedItem = oldArray[dragIndex];
-        console.log("Dragged Item : " ,draggedItem);
-        const newArray =[...oldArray];
-
-
-        return [...newArray]
-
-      });
-
-  },[])
-
-
-  //Highlight when a question is selected  
-  const onQuestionSelected = (item: FormAreaItem<ElementAttributes>) => {
-    setFormAreaItems((oldArray:any) => {
-      const index = oldArray.findIndex((i:any) => i.id === item.id);
-      if (index !== -1) {
-        oldArray.forEach((item:any) => (item.isSelected = false));
-        oldArray[index].isSelected = true;
-      }
-      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
-      return [...oldArray];
-    });
-    setSelectedItem(item);
-  };
-
-  //Drop the form element on to Form Area
-  const onFormAreaDrop = useCallback(
-    (item: FormAreaItem<ElementAttributes>) => {
-      setFormAreaItems((oldArray:any) => {
-        oldArray.forEach((item:any) => (item.isSelected = false));
-        const newArray = [
-          ...oldArray,
-          { ...item, id: nanoid(), isSelected: true },
-        ];
-        // SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, newArray);
-        return [...newArray];
-      });
-    },
-    [formAreaItems]
-  );
-
-  //Remove a form element from Form Area
-  const onElementDelete = (item: FormAreaItem<ElementAttributes>) => {
-    setFormAreaItems((oldArray:any) => {
-      oldArray.forEach((item:any) => (item.isSelected = false));
-      const newArray = oldArray.filter((oldItem:any) => oldItem.id !== item.id);
-      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, newArray);
-      return [...newArray];
-    });
-  };
-
-  //Allow the user to type in their Question to a form element
-  const onElementQuestionChanged = (
-    item: FormAreaItem<ElementAttributes>,
-    questionText: string
-  ) => {
-    setFormAreaItems((oldArray:any) => {
-      const index = oldArray.findIndex((i:any) => i.id === item.id);
-      if (index !== -1) {
-        oldArray[index].question = questionText;
-      }
-      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
-      return [...oldArray];
-    });
-  };
-
-  //Allow the user to update Options to their Checkbox or MCQ component
-  const onOptionEdit = (
-    item: FormAreaItem<ElementAttributes>,
-    option: string,
-    choiceIndex: number,
-  ) => {
-    setFormAreaItems((oldArray:any) => {
-      const index = oldArray.findIndex((i:any) => i.id === item.id);
-      if (index !== -1 && item.type == 'checkbox') {
-        const newChoice = { label: option, value: option , id:oldArray[index].attributes.choices[choiceIndex].id}
-        oldArray[index].attributes.choices[choiceIndex] = newChoice
-   //     console.log(oldArray[index].attributes.choices[choiceIndex])
-      }
-      //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
-      return [...oldArray];
-    });
-  };
-
-  //Allow the user to add Options to their Checkbox or MCQ component
-  const onOptionAdd = (
-    item:FormAreaItem<ElementAttributes>,
-
-  ) => {
-    setFormAreaItems((oldArray:any) => {
-      const newChoice = {label:"Type an Option" ,value:"Type an Option", id:nanoid() }
-
-      const index = oldArray.findIndex((i:any) => i.id === item.id);
-      if(index!==-1)
-      {
-        //oldArray[index].attributes.choices= [...oldArray[index].attributes.choices, newChoice];
-
-        oldArray[index].attributes.choices.push(newChoice)
-       //console.log(oldArray[index]);
-      
-      }
-
-      return [...oldArray]
-    });
-
-  };
-
-  const onOptionDelete = (
-    item:FormAreaItem<ElementAttributes>,
-    choiceIndex: number,
-  ) => {
-    setFormAreaItems((oldArray:any) => {
-      const index = oldArray.findIndex((i:any) => i.id === item.id);
-
-      if(index!=-1 && item.type=="checkbox")
-      { 
-        const oldChoices = oldArray[index].attributes.choices;
-        const choiceToDelete = oldChoices[choiceIndex]
-        const newChoices = oldChoices.filter((oldChoice:any)=> oldChoice.id!=choiceToDelete.id)
-
-//        console.log("New choices : ", newChoices);
-        oldArray[index].attributes.choices = newChoices;
-      }
-      
-
-      return [...oldArray]
-    });
-
-  };
-
-  const moveCard = (dragIndex: number, hoverIndex: number) => {
-    const items: any[] = JSON.parse(JSON.stringify(formAreaItems));
-    const itemToMove = items[dragIndex];
-    items.splice(dragIndex, 1);
-    items.splice(hoverIndex, 0, itemToMove);
-    setFormAreaItems(items);
-  };
-
-
-
+const Home: NextPage = () => {
+ 
+ 
+  const [email, setEmail] = useState("")
+  const [password, setpassword] = useState("")
+ 
   return (
-    <div>
+    <div className='bg-slate-900 h-screen'>
       <Head>
         <title>Form Builder Demo</title>
         <meta name="description" content="Generated by create next app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <DndProvider backend={HTML5Backend}>
-        <div className='h-full min-h-screen bg-slate-400'>
-          <div className='grid grid-cols-12 gap-x-4 '>
+ 
+<div className="m-auto container px-12 sm:px-0 mx-auto">
+  <div className="mx-auto h-full md:w-10/12 lg:w-6/12">
+    <div className="m-auto  py-12 sm:p-20 xl:w-10/12">
+      <div className="space-y-4">
+        <a href="">
+          <h2 className='text-center md:text-6xl text-4xl font-bold text-white'>Form Builder</h2>
+        </a>
 
-            {/*Element Bank  */}
-            <div className='col-span-2 box-border'>
-              <ElementBank />
-
-            </div>
-            {/*Form Area  */}
-            <div className='col-span-8 box-border'>
-              <FormArea
-                moveCard={moveCard}
-                items={formAreaItems}
-                onDrop={onFormAreaDrop}
-                onItemDelete={onElementDelete}
-                onQuestionTextChange={onElementQuestionChanged}
-                onQuestionSelected={onQuestionSelected}
-                setshowSidebar={() => { setshowSidebar(true) }}
-                onOptionEdit={onOptionEdit}
-                onOptionAdd={onOptionAdd}
-                onOptionDelete={onOptionDelete}
-                moveItem={moveItem}
+      </div>
+    <div className="mt-12 rounded-3xl border bg-gray-50 dark:border-gray-700 dark:bg-gray-800 -mx-6 sm:-mx-10 p-8 sm:p-10">
+      <div className="grid gap-6 sm:grid-cols-2">
+        <button
+          className="h-11 rounded-full border border-gray-300/75 bg-white px-6 transition active:bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:hover:bg-gray-800 dark:hover:border-gray-700"
+        >
+          <div className="flex items-center justify-center space-x-4">
+            <img src="google.svg" className="w-5" alt="" />
+            <span className="block w-max text-sm font-semibold tracking-wide text-cyan-700 dark:text-white"
+              >With Google</span
+            >
+          </div>
+        </button>
+        <button
+          className="h-11 rounded-full bg-gray-900 px-6 transition hover:bg-gray-800 focus:bg-gray-700 active:bg-gray-600 dark:bg-gray-700 dark:border dark:border-gray-600 dark:hover:bg-gray-800 dark:hover:border-gray-700"
+        >
+          <div className="flex items-center justify-center space-x-4 text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              className="w-5"
+              viewBox="0 0 16 16"
+            >
+              <path
+                d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
               />
+            </svg>
+            <span className="block w-max text-sm font-semibold tracking-wide text-white"
+              >With Github</span
+            >
+          </div>
+        </button>
+      </div>
 
-            </div>
-            {/*Element Properties  */}
-            <div className='col-span-2'>
-              <Sidebar
-                
-                selectedItem={selectedItem}
-                onItemPropertiesChange={(item) => {
-                  setFormAreaItems((oldArray:any) => {
-                    const index = oldArray.findIndex((i:any) => i.id === item.id);
-                    if (index !== -1) {
-                      oldArray[index].attributes.styling = item.attributes.styling;
-                    }
-                    //SessionStorageService.saveItem(FORM_ITEMS_SESSION_KEY, oldArray);
-                    return [...oldArray];
-                  });
-                }}
-                setshowSidebar={() => { setshowSidebar(false) }}
-                sidebarStatus={showSidebar}
-              //isSelected={selectedItem.isSelected}
-              />
-
-            </div>
-
+      <form action="" className="mt-10 space-y-8 dark:text-white">
+        <div>
+          <div className="relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400 dark:before:bg-sky-800 focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300">
+            <input value={email} onChange={(e)=> setEmail(e.target.value)} id="" type="email" placeholder="Your email or user name" className="w-full bg-transparent pb-3  border-b border-gray-300 dark:placeholder-gray-300 dark:border-gray-600 outline-none  invalid:border-red-400 transition" />
           </div>
         </div>
-      </DndProvider>
+
+        <div className="flex flex-col items-end">
+          <div className="w-full relative before:absolute before:bottom-0 before:h-0.5 before:left-0 before:origin-right focus-within:before:origin-left before:right-0 before:scale-x-0 before:m-auto before:bg-sky-400 dark:before:bg-sky-800 focus-within:before:!scale-x-100 focus-within:invalid:before:bg-red-400 before:transition before:duration-300">
+            <input value={password} onChange={(e) => setpassword(e.target.value) } id="" type="password" placeholder="Your password" className="w-full bg-transparent pb-3  border-b border-gray-300 dark:placeholder-gray-300 dark:border-gray-600 outline-none  invalid:border-red-400 transition" />
+          </div>
+          <button type="reset" className="-mr-3 w-max p-3">
+            <span className="text-sm tracking-wide text-sky-600 dark:text-sky-400">Forgot password ?</span>
+          </button>
+        </div>
+
+        <div>
+          <button
+            className="w-full rounded-full bg-sky-500 dark:bg-sky-400 h-11 flex items-center justify-center px-6 py-3 transition hover:bg-sky-600 focus:bg-sky-600 active:bg-sky-800"
+          >
+            <span className="text-base font-semibold text-white dark:text-gray-900">Login</span>
+          </button>
+          <button  type="reset" className="-ml-3 w-max p-3" >
+            <span className="text-sm tracking-wide text-sky-600 dark:text-sky-400">Create new account</span>
+          </button>
+        </div>
+      </form>
+    </div>
+      <div className="border-t pt-12 text-gray-500 dark:border-gray-800">
+        <div className="space-x-4 text-center">
+          <span>&copy; Form Builder</span>
+          <a href="#" className="text-sm hover:text-sky-900 dark:hover:text-gray-300">Contact</a>
+          <a href="#" className="text-sm hover:text-sky-900 dark:hover:text-gray-300">Privacy & Terms</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+                                   
     </div>
   )
 }
 
-Home.getInitialProps = ({req}:any ) => {
-
-  const cookies = parseCookies(req);
-
-  return {initialformAreaItems: cookies.formAreaItems};
-
-  
-};
 
 export default Home
