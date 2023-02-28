dbf6302c59e6bd28fc62c24726fe5ebfd105b864
Sounav201 <sounav2001saha@gmail.com>
2023-02-28 22:21:24 +0530

    Refixed link issue

--- pages/published/[id].tsx
+++ pages/published/[id].tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import ShortTextElement from "../../components/display-form/elements/ShortTextElement"
 import LongTextElement from '../../components/display-form/elements/LongTextElement'
 import { useRouter } from 'next/router'
@@ -7,21 +7,132 @@ import FileUploadElement from '../../components/display-form/elements/FileUpload
 import Link from 'next/link'
 import RatingElement from '../../components/display-form/elements/RatingElement'
 import Swal from 'sweetalert2'
-import {FiSend} from 'react-icons/fi';
-import Footer from '../../components/published/Footer'
-
-const Published = ({data}:any) => {
-
-  console.log('Form data from Published',data);
-  console.log('Form Heading: ', data[0].name);
-  console.log('Form Data: ', data[0].Form_data);
+// const fetchFormDataByID = async(formID:any) => {
+//   try {
+//     var endpoint = ``
+//     if(process.env.NODE_ENV == "development")
+//     {
+//       endpoint = `http://localhost:3000/api/fetchFormbyID`
+//     }
+//     else if(process.env.NODE_ENV == "production")
+//     {
+//       endpoint = `https://form-builder-demo.vercel.app/api/fetchFormbyID`
+//     }
+//     const response = await fetch(endpoint,{
+//       method:"POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({formID:formID})
+//     })
   
-  const [formData, setformData] = useState(data[0]?.Form_data || [])
-  const [formHeading, setformHeading] = useState(data[0]?.name || "Form")
+//     const res = await response.json()
+  
+//     //console.log('Data from fetchFormDataByID',data)
+//     return res.data;  
+//   } catch (error) {
+//     console.log('Error in fetchFormDataByID paths :',error)
+//   }
+  
+// }
 
+// const getPaths = async(APIendpoint) => {
+//   console.log('Endpoint : ', APIendpoint);
+//   try {
+//     const res = await fetch(APIendpoint);
+//     console.log('res status : ',res.status);
+//     const data = await res.json();
+//     console.log('Data back in getPaths: ',data)
+//     return data.data;  
+//   } catch (error) {
+//     console.log('Error in getPaths : ',error)
+//   }
+  
+// }
+
+
+// export const getStaticPaths = async() => {
+//   var endpoint = ``
+//   if(process.env.NODE_ENV === "development")
+//   {
+//     endpoint = `http://localhost:3000/api/fetchAllForms`
+//   }
+//   else if(process.env.NODE_ENV === "production")
+//   {
+//     endpoint = `https://form-builder-demo.vercel.app/api/fetchAllForms`
+//   }
+// const data = await getPaths(endpoint);
+// console.log('Data from getStaticPaths ',data);
+// const paths = data ? data.length>0 && data.map((form:any) => {
+//   return{
+//   params:{id:  form.Formid}
+//   }
+//   }) : []
+
+// return{
+//   paths,
+//   fallback:true,
+// }
+  
+// }
+
+// export async function getStaticProps(context:any) {
+//   console.log('Params id : ', context.params.id)
+//   let formID = context.params.id
+//   const data = await fetchFormDataByID(formID)
+//   console.log('Data from getStaticProps',data)
+//   return {props:{data:data}, revalidate:1}
+// }
+
+export async function getServerSideProps(context:any) {
+  console.log('Params id : ', context.params.id)
+  let formID = context.params.id
+  console.log('Form ID ', formID)
+  return {props:{formID:formID}}
+}
+
+
+const Published = ({formID}:any) => {
+
+  // console.log('Form data from Published',data || {});
+  // console.log('Form Heading: ', data[0]?.name || "" );
+  // console.log('Form Data: ', data[0]?.Form_data || "" ) ;
+  
+  // // const [formData, setformData] = useState(data[0]?.Form_data || [])
+  // const [formHeading, setformHeading] = useState(data[0]?.name || "Form")
+  const [formData, setformData] = useState([])
+  const [formHeading, setformHeading] = useState("Form")
+
+  useEffect(() => {
+    async function fetchData(formID) {
+      const res = await fetch('/api/fetchFormbyID',{
+        method:"POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({formID:formID})
+      })
+      if(res.status == 200)
+      {
+        const resData = await res.json();
+        const data = resData.data;
+        console.log('Form data: ', data);
+        
+        setformData(data[0]?.Form_data || [])
+        setformHeading(data[0]?.name || "Form")
+          
+      }
+    }
+    if(formData.length == 0)
+    { 
+      fetchData(formID)
+      
+    }
+  }, [formData])
+  
   const handleSubmit = async () => {
-    console.log("Form Submitted");
-  };
+    console.log('Form Submitted');
+  }
 
   if(formData.length == 0)
   return <div className='h-screen bg-black flex items-center justify-center text-center'>
@@ -30,109 +141,66 @@ const Published = ({data}:any) => {
     </div>
 
   return (
-    <div className="bg-cover min-h-screen flex flex-col bg-background">
-      <div className="fixed top-[4%] right-[4%]">
-      <button className="relative w-fit px-3 md:px-5 py-2 md:py-3 overflow-hidden font-medium text-lime-400 bg-transparent border border-lime-400 rounded-lg shadow-inner group delay-1000 absolute justify-end" data-aos="zoom-in-left">
-          <span className="absolute top-0 left-0 w-0 h-0 transition-all duration-200 border-t-2 border-lime-400 group-hover:w-full ease"></span>
-          <span className="absolute bottom-0 right-0 w-0 h-0 transition-all duration-200 border-b-2 border-lime-400 group-hover:w-full ease"></span>
-          <span className="absolute top-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-lime-400 group-hover:h-full ease"></span>
-          <span className="absolute bottom-0 left-0 w-full h-0 transition-all duration-300 delay-200 bg-lime-400 group-hover:h-full ease"></span>
-          <span className="absolute inset-0 w-full h-full duration-300 delay-300 bg-lime-500 opacity-0 group-hover:opacity-100"></span>
-          <span className='flex flex-row gap-1'>
-          <span className="relative transition-colors duration-300 delay-200 group-hover:text-white font-bold text-lg md:text-lg ease px-1 font-oxygen">Submit</span>
-          <FiSend className='group-hover:text-white relative transition-colors duration-300 delay-200 ease text-3xl md:text-2xl'/>
-          </span>
-          </button>
-      </div>
-      <div className="w-7/13 my-[4%] bg-violet-200 p-2 mx-auto ">
-        <div className="text-3xl font-bold my-4 text-center outline-none  w-full ">
-          {formHeading}
-        </div>
-        <form onSubmit={handleSubmit}>
-          {formData.length > 0 &&
-            formData.map((element, idx) => {
-              if (element.type == "shorttext") {
+    <div className='bg-cover min-h-screen flex flex-col bg-background'>
+              <div  className='w-7/13 my-4 bg-cover bg-violet-200 p-2 mx-auto '>
+            <div className="text-3xl font-bold my-4 text-center outline-none  w-full ">{formHeading}</div>
+            <form onSubmit={handleSubmit}>
+          {formData.length> 0 && formData.map((element,idx) => {
+            if(element.type == "shorttext")
+            {
+                return(
+                <div key={idx} className="w-10/11 mx-auto my-4">
+                    <ShortTextElement question={element.question} attributes={element.attributes} />    
+                </div>)
+            }
+            else if(element.type == "longtext")
+            {
+            return(
+            <div key={idx} className="w-10/11 mx-auto my-4">
+            
+            <LongTextElement question={element.question} attributes={element.attributes} />
+
+        </div>)
+        
+
+
+            }
+            else if(element.type == "checkbox")
+            {
                 return (
-                  <div key={idx} className="w- mx-auto my-4">
-                    <ShortTextElement
-                      question={element.question}
-                      attributes={element.attributes}
-                    />
-                  </div>
-                );
-              } else if (element.type == "longtext") {
+                    <div key={idx} className="w-10/11 mx-auto my-4">
+                        <CheckboxElement  question={element.question} attributes={element.attributes} />
+
+                    </div>
+                )
+            }
+            else if(element.type == "fileupload")
+            {
                 return (
-                  <div key={idx} className="w-10/11 mx-auto my-4">
-                    <LongTextElement
-                      question={element.question}
-                      attributes={element.attributes}
-                    />
-                  </div>
-                );
-              } else if (element.type == "checkbox") {
+                    <div key={idx} className="w-10/11 mx-auto my-4">
+                        <FileUploadElement  question={element.question} attributes={element.attributes} />
+
+                    </div>
+                )
+            }
+
+            else if(element.type == "rating")
+            {
                 return (
-                  <div key={idx} className="w-10/11 mx-auto my-4">
-                    <CheckboxElement
-                      question={element.question}
-                      attributes={element.attributes}
-                    />
-                  </div>
-                );
-              } else if (element.type == "fileupload") {
-                return (
-                  <div key={idx} className="w-10/11 mx-auto my-4">
-                    <FileUploadElement
-                      question={element.question}
-                      attributes={element.attributes}
-                    />
-                  </div>
-                );
-              } else if (element.type == "rating") {
-                return (
-                  <div key={idx} className="w-10/11 mx-auto my-4">
-                    <RatingElement
-                      question={element.question}
-                      attributes={element.attributes}
-                    />
-                  </div>
-                );
-              }
-            })}
+                    <div key={idx} className="w-10/11 mx-auto my-4">
+                    <RatingElement question={element.question} attributes={element.attributes} />
+                    </div>
+                )
+            }
+
+        })}
         </form>
-      </div>
-      <Footer />
+        </div>
+  
     </div>
-  );
-};
-
-const fetchFormDataByID = async(formID:any) => {
-  var endpoint = ``
-  if(process.env.NODE_ENV == "development")
-  {
-    endpoint = `http://localhost:3000/api/fetchFormbyID`
-  }
-  else if(process.env.NODE_ENV == "production")
-  {
-    endpoint = `https://form-builder-demo.vercel.app/pages/api/fetchFormbyID`
-  }
-  const response = await fetch(endpoint,{
-    method:"POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({formID:formID})
-  })
-
-  const res = await response.json()
-  //console.log('Data from fetchFormDataByID',data)
-  return res.data;
-}
-export async function getServerSideProps(context:any) {
-  console.log('Params id : ', context.params.id)
-  let formID = context.params.id
-  const data = await fetchFormDataByID(formID)
-//  console.log('Data from getServerSideProps',data)
-  return {props:{data:data}}
+  )
 }
 
-export default Published;
+
+
+export default Published
